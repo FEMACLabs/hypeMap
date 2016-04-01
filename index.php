cbebe0b3461e4774fda9e58f98a70a72b0b8a7a5
Andrew <andrew1burdett@gmail.com>
2016-04-01 11:05:31 -0400

    weather view complete. need to implement jquery and ajax

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include ("includes/cityWeather.php");?>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Hype Map</title>
+    
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/site.css">
+</head>
+
+<body>
+	<div class="container">
+        <div class="pure-g">
+              <div class="pure-u-1">
+                  <img src="images/logo.svg" alt="Hype Map Logo" id="logo">
+              </div>
+              <div class="pure-u-1">
+                  <form class="pure-form pure-form-stacked">
+                      <fieldset>
+                          <div class="citySelectWrapper">
+                              <img id="stateIcon" src="images/stateIcons/testState.svg">
+                              <select name="citySelect" id="citySelect">
+                                  <option value="orlando">Orlando</option>
+                                  <option value="newYork">New York</option>
+                                  <option value="sanFrancisco">San Fransisco</option>
+                                  <option value="houston">Houston</option>
+                                  <option value="denver">Denver</option>
+                              </select>
+                          </div>
+                          <a href="#" class="pure-button pure-button-primary citySubmit">Submit</a>
+                      </fieldset>
+                  </form>
+              </div>
+         </div>
+     </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="js/weather.js"></script>
+</html>
---
+++ weather.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Weather State Build</title>
+    
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/site.css">
+</head>
+
+<body>
+
+<div class="container">
+	<div class="pure-g">
+    	<div class="pure-u-1" id="menu">
+        	<a href="../hypeWeather1" class="startOver"><i class="fa fa-caret-square-o-left"></i>Start Over</a>
+            <h1 class="cityName">Orlando</h1>
+        </div>
+        <div class="pure-u-1" id="detailsContainer">
+        	<a href="#" id="detailsToggle">Details</a>
+        </div>
+        <div class="pure-u-1">
+            <div id="weatherMain">
+                <img id="conditionIcon" src="images/conditionIcons/conditionIcon.svg" alt="Condition Icon"/>
+                <p id="temp">82&deg;</p>
+                <p id="condition">Sunny</p>
+                <p id="hiLo"><span class="low">72&deg;</span> / <span class="high">93&deg;</span></p>
+            </div>
+        </div>
+        <div class="pure-u-1">
+            <div id="twitterButton">
+                <span class="twitterIcon"><i class="fa fa-twitter-square"></i></span>
+                <span class="twitterButtonText">See what others are doing...</span>
+            </div>
+        </div>
+        <div class="pure-u-1" id="activities">
+            <div id="activities">
+                <ul class="activitySlider">
+                  <li>
+                    <div id="activity1">
+                        <img class="activityImg" src="https://www.fillmurray.com/g/100/100" alt="activity1img">
+                        <div class="activityDesc">
+                            <h3>Go fLy a kite!</h3>
+                            <p>Kites are fun, kites are great... now go fly a kite.</p>
+                         </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div id="activity2">
+                        <img class="activityImg" src="https://www.fillmurray.com/g/100/100" alt="activity1img">
+                        <div class="activityDesc">
+                            <h3>Go fLy a kite!</h3>
+                            <p>Kites are fun, kites are great... now go fly a kite.</p>
+                         </div>
+                    </div>
+                  </li>
+                  <li>
+                    <div id="activity3">
+                        <img class="activityImg" src="https://www.fillmurray.com/g/100/100" alt="activity1img">
+                        <div class="activityDesc">
+                            <h3>Go fLy a kite!</h3>
+                            <p>Kites are fun, kites are great... now go fly a kite.</p>
+                         </div>
+                    </div>
+                  </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+	
+</div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="js/jquery.bxslider.min.js"></script>
+<script src="js/weather.js"></script>
+</html>
